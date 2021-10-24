828109480fe62c9810f817999dbe82398e1dd099
Nathan Barry <nathanbarry474@gmail.com>
2021-10-24 14:13:12 -0500

    added more to decentral bank contract and tests

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.5.0 <0.9.0;
 
-import './RWD.sol';
-import './MikeCoin.sol';
+import "./RWD.sol";
+import "./MikeCoin.sol";
 
 contract DecentralBank {
     string public name = "Decentral Bank";
@@ -9,8 +9,33 @@ contract DecentralBank {
     RWD public rwd;
     MikeCoin public mikecoin;
 
+    address[] public stakers;
+
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(RWD _rwd, MikeCoin _mikecoin) public {
         rwd = _rwd;
         mikecoin = _mikecoin;
+    }
+
+    function depositTokens(uint256 _amount) public {
+        // Require staking amount to be greater than 0
+        require(_amount > 0, "amount cannot be 0");
+
+        // Transfer tether tokens to this contract address for staking
+        mikecoin.transferFrom(msg.sender, address(this), _amount);
+
+        // Update Staking Balance
+        stakingBalance[msg.sender] += _amount;
+
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        // Update Staking Status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
 }
--- test/decentralBank.tests.js
+++ test/decentralBank.tests.js
@@ -1,56 +1,69 @@
-const MikeCoin = artifacts.require('MikeCoin');
-const RWD = artifacts.require('RWD');
-const DecentralBank = artifacts.require('DecentralBank');
+const MikeCoin = artifacts.require("MikeCoin");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
 
-require('chai')
-.use(require('chai-as-promised'))
-.should();
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
 
-contract('DecentralBank', ([owner, customer]) => {
-    let mikecoin, rwd;
+contract("DecentralBank", ([owner, customer]) => {
+  let mikecoin, rwd;
 
-    function tokens(number) {
-        return web3.utils.toWei(number, 'ether');
-    }
+  function tokens(number) {
+    return web3.utils.toWei(number, "ether");
+  }
 
-    before(async () => {
-        // load Contracts
-        mikecoin = await MikeCoin.new();
-        rwd = await RWD.new();
-        decentralBank = await DecentralBank.new(rwd.address, mikecoin.address);
+  before(async () => {
+    // load Contracts
+    mikecoin = await MikeCoin.new();
+    rwd = await RWD.new();
+    decentralBank = await DecentralBank.new(rwd.address, mikecoin.address);
 
-        //Transfer all tokens to DecentralBank (1 million)
-        await rwd.transfer(decentralBank.address, tokens('1000000'));
+    //Transfer all tokens to DecentralBank (1 million)
+    await rwd.transfer(decentralBank.address, tokens("1000000"));
 
-        // Transfer 100 mock Tethers to Customer
-        await mikecoin.transfer(customer, tokens('100')), {from: owner};
-    })
+    // Transfer 100 mock Tethers to Customer
+    await mikecoin.transfer(customer, tokens("100")), { from: owner };
+  });
 
-    describe('Mock MikeCoin Deployment', async () =>  {
-        it('matches name successfully', async () => {
-            const name = await mikecoin.name();
-            assert.equal(name, 'MikeCoin');
-        })
-    })
-    describe('RWD Deployment', async () =>  {
-        it('matches name successfully', async () => {
-            const name = await rwd.name();
-            assert.equal(name, 'Reward Token');
-        })
-        it('matches symbol successfully', async () => {
-            const symbol = await rwd.symbol();
-            assert.equal(symbol, 'RWD');
-        })
-    })
-    describe('Decentral Bank Deployment', async () =>  {
-        it('matches name successfully', async () => {
-            const name = await decentralBank.name();
-            assert.equal(name, 'Decentral Bank');
-        })
+  describe("Mock MikeCoin Deployment", async () => {
+    it("matches name successfully", async () => {
+      const name = await mikecoin.name();
+      assert.equal(name, "MikeCoin");
+    });
+  });
+  describe("RWD Deployment", async () => {
+    it("matches name successfully", async () => {
+      const name = await rwd.name();
+      assert.equal(name, "Reward Token");
+    });
+    it("matches symbol successfully", async () => {
+      const symbol = await rwd.symbol();
+      assert.equal(symbol, "RWD");
+    });
+  });
+  describe("Decentral Bank Deployment", async () => {
+    it("matches name successfully", async () => {
+      const name = await decentralBank.name();
+      assert.equal(name, "Decentral Bank");
+    });
 
-        it('there are tokens', async () => {
-            balance = await rwd.balanceOf(decentralBank.address);
-            assert.equal(balance, tokens('1000000'))
-        })
-    })
-})
+    it("there are tokens", async () => {
+      balance = await rwd.balanceOf(decentralBank.address);
+      assert.equal(balance, tokens("1000000"));
+    });
+  });
+  describe("Yield Farming", async () => {
+    it("rewards tokens for staking", async () => {
+      let result;
+
+      // Check Investor Blanace
+      result = await mikecoin.balanceOf(customer);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "customer mock wallet balance before staking"
+      );
+    });
+  });
+});
